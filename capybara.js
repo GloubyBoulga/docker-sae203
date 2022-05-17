1a1a6fb10ae0dd51523121c62e8d7e132f05faad
Paul Gallon <popolilipopo35@gmail.com>
2022-05-17 11:20:24 +0200

    Modification des 3 composants avec ajout de video

--- capybara.js
+++ capybara.js
@@ -1,15 +1,15 @@
 // JavaScript code
-function search_animal() {
+function search_video() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('animals');
+    let x = document.getElementsByClassName('video');
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
         }
         else {
-            x[i].style.display="list-item";                 
+            x[i].style.display="list-item";                
         }
     }
 }
